3f9692bc460f18e92aa934bf18d850e591d8c6e4
Grant Dempsey <CryptoDemps@gmail.com>
2020-06-03 10:52:13 -0600

    add_ingredient method complete with testing

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -12,9 +12,19 @@ class RecipeTest < Minitest::Test
 
   def test_recipe_holds_ingredients
     recipe1 = Recipe.new("Mac and Cheese")
+    assert_equal "Mac and Cheese", recipe1.name
+    assert_equal ({}), recipe1.ingredients_required
+  end
+
+  def test_add_ingredient
+    recipe1 = Recipe.new("Mac and Cheese")
     ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
-    assert_equal "Mac and Cheese", recipe1.name
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+    assert_equal 6, recipe1.ingredients_required[ingredient1]
+    assert_equal 8, recipe1.ingredients_required[ingredient2]
   end
 
 
